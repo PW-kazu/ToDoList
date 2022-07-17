a4edaf00829781f7f0bc407592a17ffa354c5bfe
PW kazu <komura_jutasrou@icloud.com>
2022-07-17 15:53:07 +0900

    修正

--- app/Models/ToDoListModel.php
+++ app/Models/ToDoListModel.php
@@ -2,40 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+
+
 
 class ToDoListMOdel extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'content',
-    ];
+    protected $fillable = ['content',];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'id',
-    ];
+    protected $hidden = ['id',];
 
     /**
      * The attributes that should be cast.
      *
-     * @var array<string, string>
+     * 
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    
 }
